7359a2c8621bc18c1c53f68353dd4a2c67c44164
Reginaldo Boeke <reginaldoboeke@MacBook-Pro-de-Reginaldo.local>
2020-10-14 20:39:42 -0300

    Finalizado backend do app

---
+++ src/config/upload.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+
+import multer from 'multer';
+
+export default {
+  storage: multer.diskStorage({
+    destination: path.join(__dirname, '..', '..', 'uploads'),
+    filename: (request, file, cb) => {
+      const fileName = `${Date.now()}-${file.originalname}`;
+
+      cb(null, fileName);
+    }
+  })
+}
